77781bcc620c9739b8f4df6a5b18a3e9856f3a16
KitteeRusty <108312471+KitteeRusty@users.noreply.github.com>
2024-07-29 14:39:13 +0800

    deploy

--- vite.config.ts
+++ vite.config.ts
@@ -2,12 +2,10 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
-export default defineConfig(({ mode }) => {
-  return {
-    plugins: [vue()],
-    base: '/website', // 替换为您的仓库名称
-    build: {
-      outDir: 'dist'
-    }
+export default defineConfig({
+  plugins: [vue()],
+  base: '/website', // 替换为您的仓库名称
+  build: {
+    outDir: 'dist'
   }
 })
